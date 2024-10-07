a300aef5d8863eae987f7d4c2c5ca06856295556
fwliu1 <46015578+fwliu1@users.noreply.github.com>
2024-10-07 19:12:15 +0000

    pulling

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,7 @@ def get_gemini_response(model, question):
 st.title("Gemini AI Q&A App")
 
 # API key input
-api_key = st.secrets["APIKEY"]
-#text_input("Enter your Gemini API Key:", type="password")
+api_key = st.text_input("Enter your Gemini API Key:", type="password")
 
 if api_key:
     # Initialize the model
